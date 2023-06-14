fabcf394275d0aba7f9a783d13b4a1a9a38808dc
Susie Gordon <susieyoungchoi@gmail.com>
2023-06-14 10:02:47 -0400

    added food.js database and created new route to display individual food items

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const port = 3000;
+const port = 3200;
 const app = express();
 const drinks = require("./models/drinks"); // return what drinks.js exports
+const food = require("./models/food")
+
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Gitpub App!");
@@ -9,18 +11,27 @@ app.get("/", (req, res) => {
 
 // Index Route
 app.get("/drinks", (req, res) => {
-    res.render("index.ejs", {drinks});
+    res.render("index.ejs", {drinks, food});
 });
+
 
 // Show Route
 app.get("/drinks/:id", (req, res) => {
     const id = req.params.id;
     const drink = drinks[id];
     // res.send(req.params.id)
-    res.render("show.ejs", {drink});
+    const foodItem = food[id]
+    res.render("show.ejs", {drink, foodItem});
+});
+
+app.get("/food/:id", (req, res) => {
+    const id = req.params.id;
+    const foodItem = food[id];
+    res.render("show_food.ejs", {foodItem});
 });
 
 
-app.listen(3000, ()=> {
+
+app.listen(3200, ()=> {
     console.log(`Listening on ${port}`);
 });
